894ae7993e3c1f778923251772497bfa578d3058
Andrea Tomasi <andrea.tomasi@argo.vision>
2019-05-18 11:48:18 +0200

    Callback on end

--- sjson_test.py
+++ sjson_test.py
@@ -26,6 +26,27 @@ class TestInit(unittest.TestCase):
         
         self.assertTrue(is_called)
 
+    def test_end_callback_is_called_on_empty(self):
+        is_called = False
+        def end():
+            nonlocal  is_called
+            is_called = True
+            
+        data = uio.StringIO("")
+        sjson.loads(data,None,end)
+        
+        self.assertTrue(is_called)
+
+    def test_end_callback_is_called(self):
+        is_called = False
+        def end():
+            nonlocal  is_called
+            is_called = True
+            
+        data = uio.StringIO("ciao")
+        sjson.loads(data,None,end)
+        
+        self.assertTrue(is_called)
 
 
 if __name__ == '__main__':
